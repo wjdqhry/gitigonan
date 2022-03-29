d6c7b1f510831caf9aaf6eddca288af6d35d64fe
wjdqhry <yung12320@daum.net>
2022-03-29 11:39:48 +0900

    add: save cli result

---
+++ src/cli.ts
@@ -0,0 +1,55 @@
+import * as fs from "fs";
+import path from "path";
+import { parseIgonan } from "./parser";
+
+import { newLine, printIgonan, printSorry, SorryType } from "./printer";
+
+function getDirPath(folderName: string) {
+  const root = process.cwd();
+  return path.join(root, folderName);
+}
+
+export function runCommander() {
+  var commander = require("commander");
+
+  commander
+    .command("igonan")
+    .usage("--type [type]")
+    .description("깃아이고난 를! 만들어주는 나")
+    .alias("tmpl")
+    .option("-t, --type [type]", "언어나 프레임워크 를! 고르는 나")
+    .action(getIgonanAction);
+
+  commander.command("*", { noHelp: true }).action(() => {
+    console.log("명령어 를! 찾을 수 없는 나");
+    commander.help();
+  });
+
+  commander.parse(process.argv);
+}
+
+const getIgonanAction = async (_: any, { args }: any) => {
+  console.log("자~");
+
+  const type = args[0];
+  if (!type) {
+    await printSorry(SorryType.ParamNotFound);
+    return;
+  }
+
+  try {
+    const result = await parseIgonan(type);
+
+    if (result === "" || result.includes("undefined")) {
+      await printSorry(SorryType.IgonanNotFound);
+    } else {
+      await printIgonan();
+      fs.writeFileSync(".gitigonan", result);
+    }
+  } catch (e: any) {
+    await printSorry(SorryType.IgonanNotFound);
+  }
+
+  newLine();
+  console.log("옹!!!!~");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,7 @@
-import * as fs from "fs";
-import path from "path";
-import { parseIgonan } from "./parser";
+import { runCommander } from "./cli";
 
-import {
-  delayedPrint,
-  sleep,
-  newLine,
-  printIgonan,
-  printSorry,
-  SorryType,
-} from "./printer";
-
-function getDirPath(folderName: string) {
-  const root = process.cwd();
-  return path.join(root, folderName);
+async function runCli() {
+  runCommander();
 }
 
-var commander = require("commander");
-
-commander
-  .command("igonan")
-  .usage("--type [type]")
-  .description("깃아이고난 를! 만들어주는 나")
-  .alias("tmpl")
-  .option("-t, --type [type]", "언어나 프레임워크 를! 고르는 나", "index")
-  .action(async (_: any, { args }: any) => {
-    console.log("자~");
-
-    const type = args[0];
-    if (!type) {
-      await printSorry(SorryType.ParamNotFound);
-      return;
-    }
-
-    try {
-      const result = await parseIgonan(type);
-      // TODO: 저장 하자
-      await printIgonan();
-      
-    } catch (e: any) {
-      await printSorry(SorryType.IgonanNotFound);
-    }
-
-    newLine();
-    console.log("옹!!!!~");
-  });
-
-commander.command("*", { noHelp: true }).action(() => {
-  console.log("해당 명령어를 찾을 수 없습니다.");
-  commander.help();
-});
-
-commander.parse(process.argv);
+runCli();
